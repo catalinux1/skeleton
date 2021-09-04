8a5c7c2df94e2a88951770edcb670737234dc37b
catalin <catalin.mihaila@esolutions.ro>
2021-09-04 14:03:50 +0300

    simple auth success

---
+++ README.md
@@ -0,0 +1,3 @@
+Superadmin: superadmin@skeleton.ro
+Parola: superadminus
+Parola hash: $2a$12$xvWBoCPRPUUO74jSCfXjce5jgil.sEK9CJ82ki11K1BiUr5GBhUy6
--- src/main/resources/db/migration/V1__user_shema.sql
+++ src/main/resources/db/migration/V1__user_shema.sql
@@ -22,6 +22,6 @@ create TABLE user_role (
 create sequence seq_user_role start with 1 increment by 1 NO MINVALUE NO MAXVALUE CACHE 1;
 
 INSERT INTO backend_user (id, email, password, first_name, last_name, active, created, created_by, last_update, last_update_by)
- VALUES (-1, 'superadmin@skeleton.ro', '$2a$10$8jUuBLagifRSiETCjGJSuu4wPSvBptkC74ebCVnTL53WtBHcLWzhW', 'Superadmin', 'Superadmin', true, '2021-09-04 15:12:01.621129', 'superadmin@skeleton.ro', '2021-09-04 15:12:01.621129', 'superadmin@skeleton.ro');
+ VALUES (-1, 'superadmin@skeleton.ro', '$2a$12$xvWBoCPRPUUO74jSCfXjce5jgil.sEK9CJ82ki11K1BiUr5GBhUy6', 'Superadmin', 'Superadmin', true, '2021-09-04 15:12:01.621129', 'superadmin@skeleton.ro', '2021-09-04 15:12:01.621129', 'superadmin@skeleton.ro');
 
 INSERT INTO user_role (id, user_id, role) VALUES (-1, -1, 'ROLE_SUPERADMIN');
