d9bd6c063265ae4d88821eae60155b7633d5d1e7
catalin <catalin.mihaila@esolutions.ro>
2021-09-07 18:56:44 +0300

    reorganize user management

--- src/main/java/com/skeleton/controller/UserManagementController.java
+++ src/main/java/com/skeleton/controller/UserManagementController.java
@@ -1,12 +1,8 @@
 package com.skeleton.controller;
 
-import com.skeleton.service.BranchService;
-import com.skeleton.service.UserService;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,15 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/userManagement")
 public class UserManagementController {
-    @NonNull
-    private final UserService userService;
-    private final BranchService branchService;
 
-    @GetMapping(value = "/list")
-    public String getList(final Model model) {
-        model.addAttribute("branchModel", branchService.getBranchModel());
-        model.addAttribute("userModel", userService.getUserModelList());
+    @GetMapping(value = "/dashboard")
+    public String getList() {
 
-        return "userManagement/list";
+        return "userManagement/dashboard";
+    }
+
+    @GetMapping(value = "/branchList")
+    public String getBranchPage() {
+
+        return "userManagement/branchList";
     }
 }
