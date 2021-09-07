0880c746c89de8736453eb5b4eaf858d1978b360
catalin <catalin.mihaila@esolutions.ro>
2021-09-07 23:47:59 +0300

    branch pagination

--- src/main/java/com/skeleton/controller/UserManagementController.java
+++ src/main/java/com/skeleton/controller/UserManagementController.java
@@ -1,8 +1,15 @@
 package com.skeleton.controller;
 
+import com.skeleton.service.BranchService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/userManagement")
 public class UserManagementController {
+    @NonNull
+    private final BranchService branchService;
 
     @GetMapping(value = "/dashboard")
     public String getList() {
@@ -18,9 +27,12 @@ public class UserManagementController {
         return "userManagement/dashboard";
     }
 
-    @GetMapping(value = "/branchList")
-    public String getBranchPage() {
+    @GetMapping(value = "/branchPage")
+    public String getBranchPage(final Model model,
+                                @SortDefault(sort = "name", direction = Sort.Direction.ASC)
+                                @PageableDefault(size = 10) final Pageable pageable) {
+        model.addAttribute("branchModelPage", branchService.getBranchModelPage(pageable));
 
-        return "userManagement/branchList";
+        return "userManagement/branchPage";
     }
 }
--- src/main/java/com/skeleton/service/BranchService.java
+++ src/main/java/com/skeleton/service/BranchService.java
@@ -1,16 +1,21 @@
 package com.skeleton.service;
 
+import com.skeleton.entity.Branch;
 import com.skeleton.mapper.BranchMapper;
 import com.skeleton.model.BranchModel;
 import com.skeleton.repository.BranchRepository;
+import com.skeleton.util.PageWrapper;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -20,10 +25,14 @@ public class BranchService {
     @NonNull
     private final BranchRepository branchRepository;
 
-    public List<BranchModel> getBranchModel() {
-        return branchRepository.findAll()
-                .stream()
-                .map(BranchMapper::entityToModel)
-                .collect(Collectors.toList());
+    public PageWrapper<BranchModel> getBranchModelPage(final Pageable pageable) {
+        final List<BranchModel> branchModelList = new ArrayList<>();
+        final Page<Branch> branchPage= branchRepository.findAll(pageable);
+        for (final Branch branch : branchPage.getContent()) {
+            final BranchModel branchModel = BranchMapper.entityToModel(branch);
+            branchModelList.add(branchModel);
+        }
+
+        return new PageWrapper<>(new PageImpl<>(branchModelList, pageable, branchPage.getTotalElements()), "/userManagement/branchPage");
     }
 }
