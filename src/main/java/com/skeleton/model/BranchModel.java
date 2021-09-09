ad61d204bbdc22c28488103ac35099e1589c2976
catalin <catalin.mihaila@esolutions.ro>
2021-09-09 20:39:02 +0300

    branch validation message

--- src/main/java/com/skeleton/model/BranchModel.java
+++ src/main/java/com/skeleton/model/BranchModel.java
@@ -15,8 +15,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BranchModel {
     private Long id;
-    @NotBlank()
-    @Size(min = 2, max = 255)
+    @NotBlank(message = "Campul trebuie completat!")
+    @Size(min = 2, max = 255, message = "Campul trebuie sa contina intre 2 si 255 caractere!")
     private String name;
     private Boolean active;
     private String lastUpdateBy;
